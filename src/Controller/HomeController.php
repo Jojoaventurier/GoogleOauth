f4f2aa7cad85e516fcc08c8d6a41db1551aa42ba
Geoffroy Maurutto <165882140+Jojoaventurier@users.noreply.github.com>
2024-07-20 18:41:48 +0200

    updts

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,19 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+
+#[IsGranted("ROLE_USER")]
 class HomeController extends AbstractController
-{
-    #[Route('/home', name: 'app_home')]
+
+{   
+    #[Route('', name: 'index')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('home/index.html.twig');
     }
 }
